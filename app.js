21cb3024cace433031eba929c40995a118b78b88
edreesfazel <134668954+edreesfazel@users.noreply.github.com>
2023-06-28 23:39:17 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=AJjnS-Ja_Yo");
+});
+
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=4JzwOPOvk9k");
+});
+
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=jR4AG5LdKYE");
+});
+
+document.getElementsByClassName("fa-brands fa-youtube")[3].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=E3GZojp5Bo4");
+});
